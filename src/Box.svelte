9025f9fac715730e352d868db3f9c28ae908f498
Evie <evita@stenqvist.co>
2022-04-13 12:26:19 +0200

    Initial commit: basic mint flow

---
+++ src/Box.svelte
@@ -0,0 +1,28 @@
+<section>
+  <slot />
+</section>
+
+<style>
+  section {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+    background: white;
+    border-radius: 2px;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    min-width:320px;
+    width: 750px;
+    height: 350px;
+    border: black solid 4px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  @media (max-width: 640px) {
+    section {
+      max-width: none;
+    }
+  }
+</style>
---
+++ src/Main.svelte
@@ -0,0 +1,90 @@
+<script>
+  import Box from "./Box.svelte";
+  import ExplodeButton from "./ExplodeButton.svelte";
+
+  let isMinting = false;
+  let success = false;
+
+  // TODO connect mint
+  async function mint() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        isMinting = false;
+        success = true;
+
+        console.log(isMinting);
+        console.log(success);
+        resolve();
+      }, 3000);
+    });
+  }
+
+  let onClick = async () => {
+    if (!isMinting) {
+      isMinting = true;
+      await mint();
+    }
+  };
+</script>
+
+<main>
+  <Box>
+    {#if isMinting}
+      <h1>Transaction pending...</h1>
+      <!-- TODO href -->
+      <p>See transaction <a href="">https://polygon...</a></p>
+    {:else if success}
+      <h1>CONGRATS 🎉 🍾</h1>
+      <p>You have minted 1 Easter NFT!</p>
+    {:else}
+      <h1>Mint <b>Bankless.se</b> Easter NFT 🐣</h1>
+      <p>Each NFT costs only x MATIC, but you can only have 100 🙀</p>
+      <ExplodeButton {onClick} />
+    {/if}
+  </Box>
+</main>
+
+<style>
+  :global(body) {
+    background: #c6ffdd; /* fallback for old browsers */
+    background: -webkit-linear-gradient(
+      to left,
+      #f7797d,
+      #fbd786,
+      #c6ffdd
+    ); /* Chrome 10-25, Safari 5.1-6 */
+    background: linear-gradient(
+      to left,
+      #f7797d,
+      #fbd786,
+      #c6ffdd
+    ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  }
+
+  main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+  h1 {
+    color: red;
+    text-transform: uppercase;
+    font-size: 3em;
+    font-weight: 100;
+  }
+
+  b {
+    color: black;
+    text-decoration: underline red;
+  }
+
+  p {
+    font-size: 1.5em;
+    font-weight: 100;
+    font-family: monospace;
+  }
+</style>
