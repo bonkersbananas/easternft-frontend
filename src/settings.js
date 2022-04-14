9f040d13aa490f4bce4eb4fc7a788fb89653cdba
Evie <evita@stenqvist.co>
2022-04-14 23:00:18 +0200

    Live contract

--- src/settings.js
+++ src/settings.js
@@ -1,9 +1,9 @@
 const networks = {
-  MAINNET: { chainId: 137, contractAddress: "0x00" },
+  MAINNET: { chainId: 137, contractAddress: "0x75f61dd2443e62b42034696bb3d6b4183b574de9" },
   MUMBAI: {
     chainId: 80001,
-    contractAddress: "0x50964bc87986668Ce8468dCDd0bbc0Bab62d9762",
+    contractAddress: "0xF2D72aa36F40643CF24D9D38eF7e6cc6D0738446",
   },
 };
 
-export const CURRENT_NETWORK = networks["MUMBAI"];
+export const CURRENT_NETWORK = networks["MAINNET"];
